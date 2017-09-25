7b8ae27bc0de41f5d1b8a424a8bb42be34974b26
Ted M. Young <tedyoung@gmail.com>
2017-09-25 16:58:19 -0500

    Implemented basic deposit to account task.

---
+++ src/main/java/com/visa/ncg/canteen/Account.java
@@ -0,0 +1,14 @@
+package com.visa.ncg.canteen;
+
+public class Account {
+
+  private int balance = 0;
+
+  public void deposit(int amount) {
+    balance += amount;
+  }
+
+  public int balance() {
+    return balance;
+  }
+}
---
+++ src/test/java/com/visa/ncg/canteen/AccountDepositTest.java
@@ -0,0 +1,37 @@
+package com.visa.ncg.canteen;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountDepositTest {
+
+  @Test
+  public void newAccountsHaveZeroBalance() throws Exception {
+    assertThat(new Account().balance())
+        .isZero();
+  }
+
+  @Test
+  public void deposit10DollarsToNewAccountResultsInAccountHaving10Dollars() throws Exception {
+
+    Account account = new Account();
+
+    account.deposit(10);
+
+    assertThat(account.balance())
+        .isEqualTo(10);
+  }
+
+  @Test
+  public void twoDepositsOf10And5DollarsResultInAccountHaving15Dollars() throws Exception {
+    Account account = new Account();
+
+    account.deposit(10);
+    account.deposit(5);
+
+    assertThat(account.balance())
+        .isEqualTo(15);
+  }
+
+}
