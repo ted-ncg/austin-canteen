62491ab61ee692378e2d147f4c2460cbe410b6cb
igarza <igarza@visa.com>
2017-09-26 11:12:12 -0500

    added new InsufficientBalanceException for when withdraw amount greater than balance

--- src/main/java/com/visa/ncg/canteen/Account.java
+++ src/main/java/com/visa/ncg/canteen/Account.java
@@ -1,5 +1,7 @@
 package com.visa.ncg.canteen;
 
+import java.security.InvalidParameterException;
+
 public class Account {
 
   private int balance = 0;
@@ -8,9 +10,14 @@ public class Account {
     balance += amount;
   }
 
-  public void withdraw(int amount) {
-    if(amount > balance) {
+  public void withdraw(int amount) throws InsufficientBalanceException {
+    if(amount < 0) {
       throw new IllegalArgumentException();
+    }
+
+    else if(amount > balance) {
+        throw new InsufficientBalanceException();
+
     } else {
       balance -= amount;
     }
--- src/test/java/com/visa/ncg/canteen/AccountWithdrawTest.java
+++ src/test/java/com/visa/ncg/canteen/AccountWithdrawTest.java
@@ -1,7 +1,11 @@
 package com.visa.ncg.canteen;
 
 import org.junit.Test;
+
+import java.security.InvalidParameterException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class AccountWithdrawTest {
@@ -11,16 +15,34 @@ public class AccountWithdrawTest {
         Account account = new Account();
 
         account.deposit(70);
-        account.withdraw(30);
+        try {
+            account.withdraw(30);
+        } catch (InsufficientBalanceException e) {
+            e.printStackTrace();
+        }
 
         assertThat(account.balance()).isEqualTo(40);
     }
-   @Test (expected = IllegalArgumentException.class)
-    public void withdrraw50FromAccountWith20ResultsError() {
+
+    @Test
+    public void withdraw50FromAccountWith20ResultsError() {
         Account account = new Account();
 
         account.deposit(20);
-        account.withdraw(50);
 
+        assertThatThrownBy(() -> {account.withdraw(50);}).isInstanceOf(InsufficientBalanceException.class);
+
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void withdrawNegativeAmountResultsInError() {
+        Account account = new Account();
+
+        account.deposit(20);
+        try {
+            account.withdraw(-10);
+        } catch (InsufficientBalanceException e) {
+            e.printStackTrace();
+        }
     }
 }
