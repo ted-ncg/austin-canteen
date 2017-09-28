cd9fa759f113ddd8d092dadd4f7993e62729e866
Ted M. Young <tedyoung@gmail.com>
2017-09-28 11:02:56 -0500

    Solution for Lab #8, with integration test passing.

--- src/main/java/com/visa/ncg/canteen/AccountWebController.java
+++ src/main/java/com/visa/ncg/canteen/AccountWebController.java
@@ -1,16 +1,31 @@
 package com.visa.ncg.canteen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AccountWebController {
 
+  private final AccountRepository accountRepository;
+
+  @Autowired
+  public AccountWebController(AccountRepository accountRepository) {
+    this.accountRepository = accountRepository;
+  }
+
   @GetMapping("/account/{id}")
-  public String viewAccount(Model model) {
-    // retrieve account from repository
-    // convert it to accountResponse (POJO)
+  public String viewAccount(@PathVariable("id") String id,
+      Model model) {
+    Account account = accountRepository.findOne(Long.valueOf(id));
+
+    AccountResponse accountResponse = new AccountResponse();
+    accountResponse.setId(account.getId());
+    accountResponse.setBalance(account.balance());
+
+    model.addAttribute("account", accountResponse);
     return "account-view";
   }
 
